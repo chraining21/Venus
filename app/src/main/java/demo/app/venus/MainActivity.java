704821cb4396ad83f10f5a749fbf0092dd543d39
Muubye <95389637+Muubye@users.noreply.github.com>
2022-08-20 15:20:43 +0800

    膚質測驗

--- app/src/main/java/demo/app/venus/MainActivity.java
+++ app/src/main/java/demo/app/venus/MainActivity.java
@@ -2,13 +2,25 @@ package demo.app.venus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.setting);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SettingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
