b09aa3bdbd5299a34edca71f011f4e1adfcbba7a
lan <kitty90508s@gmail.com>
2022-08-20 19:48:31 +0800

    修改清單那頁

--- app/src/main/java/demo/app/venus/SettingActivity.java
+++ app/src/main/java/demo/app/venus/SettingActivity.java
@@ -1,29 +1,45 @@
 package demo.app.venus;
 
-import androidx.appcompat.app.AppCompatActivity;
+//import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.RelativeLayout;
 
-public class SettingActivity extends AppCompatActivity {
-
-    Button button;
+public class SettingActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
-        button = findViewById(R.id.userGuideView);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SettingActivity.this,UserGuideActivity.class);
-
-                startActivity(intent);
+        //觸發'產品清單'
+        ((RelativeLayout)findViewById(R.id.listLayout)).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                        startActivityForResult(new Intent(SettingActivity.this,ListActivity.class), 0);
             }
         });
+        //觸發'分析成分'
+        ((RelativeLayout)findViewById(R.id.scanLayout)).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                startActivityForResult(new Intent(SettingActivity.this,ListActivity.class), 0);
+            }
+        });
+        //觸發'膚質測驗'
+        ((RelativeLayout)findViewById(R.id.scanLayout)).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                startActivityForResult(new Intent(SettingActivity.this,ListActivity.class), 0);
+            }
+        });
+
+        //觸發'使用說明'
+        ((RelativeLayout)findViewById(R.id.userGuideLayout)).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                        startActivityForResult(new Intent(SettingActivity.this,UserGuideActivity.class), 0);
+            }
+        });
+
     }
 }
